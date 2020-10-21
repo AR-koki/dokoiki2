737e76a5b3b1f5cc47b38ffd0fd75e0501bcbe20
vagrant <vagrant@localhost.localdomain>
2020-10-21 00:03:14 +0000

    [Fix]layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,4 @@
 class HomesController < ApplicationController
   def top
-    @posts = Post.all
-    @user = current_user
-    @recommendeds = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post))
-  end
-
-  def about
   end
 end
